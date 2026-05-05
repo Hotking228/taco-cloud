88c7cddbcf0031d3d59fe06586242898f644f8b5
hotking <kraulov@bk.ru>
2026-05-05 10:26:39 +0300

    Основные Rest методы

--- src/main/java/tacos/controller/TacoController.java
+++ src/main/java/tacos/controller/TacoController.java
@@ -7,11 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tacos.entity.Taco;
+import tacos.entity.TacoOrder;
 import tacos.repository.TacoRepository;
 
 import java.util.Optional;
 
-@RestController
+@RestController //Все возвращаемые значения должны вставляться в тело ответа
+    // а не переноситься в модель для отображения
 @RequestMapping(path = "/api/tacos",
                 produces = "application/json")//Указываем возвращаемый тип
 @CrossOrigin(origins = "https://tacocloud:8443")//Разрешает межсайтовые запросы
@@ -20,7 +22,7 @@ public class TacoController {
 
     private final TacoRepository tacoRepo;
 
-    @GetMapping(params = "recent")
+    @GetMapping(params = "recent") //Получаем объект от сервера
     public Iterable<Taco> recentTacos(){
         PageRequest page = PageRequest.of(
                 0, 12, Sort.by("createdAt").descending());
@@ -35,5 +37,13 @@ public class TacoController {
         }
 
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(consumes = "application/json") //Кладем новый объект на сервер
+    @ResponseStatus(HttpStatus.CREATED)//Возвращаем статус 201(CREATED) вместо 200(OK)
+    public Taco postTaco(@RequestBody Taco taco){//Указывает что тело запроса
+                            //должно быть преобразовано в объект
+                            //Taco
+        return tacoRepo.save(taco);
     }
 }
