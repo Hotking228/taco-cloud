c37530e29f734c497db0c3fa660fd1e1e9fa6c45
hotking <kraulov@bk.ru>
2026-05-08 13:54:56 +0300

    Java DSL конфигурация для интеграции

--- src/main/java/tacos/config/FileWriterIntegrationConfig.java
+++ src/main/java/tacos/config/FileWriterIntegrationConfig.java
@@ -6,7 +6,12 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.core.GenericTransformer;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlowBuilder;
+import org.springframework.integration.dsl.IntegrationFlowDslKt;
+import org.springframework.integration.dsl.MessageChannels;
 import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.dsl.Files;
 import org.springframework.integration.file.support.FileExistsMode;
 
 import java.io.File;
@@ -15,20 +20,14 @@ import java.io.File;
 public class FileWriterIntegrationConfig {
 
     @Bean
-    @Transformer(inputChannel = "textInChannel",//Преобразователь
-                 outputChannel = "fileWriterChannel")
-    public GenericTransformer<String, String> upperCaseTransformer(){
-        return text -> text.toUpperCase();
-    }
-
-    @Bean
-    @ServiceActivator(inputChannel = "fileWriterChannel")//Обработчик записи в файл
-    public FileWritingMessageHandler fileHandle(){
-        FileWritingMessageHandler handler =
-                new FileWritingMessageHandler(new File("/tmp/sia6/files"));
-        handler.setExpectReply(false);
-        handler.setFileExistsMode(FileExistsMode.REPLACE);
-        handler.setAppendNewLine(true);
-        return handler;
+    public IntegrationFlow fileWriterFlow(){
+        return IntegrationFlow
+                .from(MessageChannels.direct("textInChannel"))
+                .<String, String>transform(t -> t.toUpperCase())
+                .handle(Files
+                        .outboundAdapter(new File("/tmp/sia6/files"))
+                        .fileExistsMode(FileExistsMode.REPLACE)
+                        .appendNewLine(true))
+                .get();
     }
 }
